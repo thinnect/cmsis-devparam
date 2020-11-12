a40fcabfbc53d4374a42b978a491115a8aa566d0
German <german.mundinger@gmail.com>
2020-11-12 11:48:43 +0200

    Fix I2C

--- src/params/devp_ltc.c
+++ src/params/devp_ltc.c
@@ -9,6 +9,9 @@
 
 #include "stddef.h"
 
+#include "retargeti2cconfig.h"
+#include "platform_i2c.h"
+
 #include "ltc.h"
 
 // -----------------------------------------------------------------------------
@@ -16,7 +19,7 @@ static int dp_ltc_battery_voltage_get (devp_t * param, void * value);
 
 static devp_t m_dp_ltc_battery_voltage =
 {
-    .name = "battery_voltage(mV)",
+    .name = "battery_voltage_(mV)",
     .type = DP_TYPE_UINT16,
     .size = sizeof(uint16_t),
     .persist = false,
@@ -26,17 +29,18 @@ static devp_t m_dp_ltc_battery_voltage =
 
 int dp_ltc_battery_voltage_get (devp_t * param, void * value)
 {
-    const bool is_ltc_initialized = ltc4015_init();
+    *((uint16_t*)value) = 0U;
 
-    if (true == is_ltc_initialized)
+    if(true == platform_i2c_request(RETARGET_I2C_DEV, 1000U))
     {
-        *((uint16_t*)value) = ltc4015_battery_read();
-    }
-    else
-    {
-        *((uint16_t*)value) = 0U;
-    }
+        const bool is_ltc_initialized = ltc4015_init();
 
+        if (true == is_ltc_initialized)
+        {
+            *((uint16_t*)value) = ltc4015_battery_read();
+        }
+        platform_i2c_release(RETARGET_I2C_DEV);
+    }
     return sizeof(uint16_t);
 }
 // -----------------------------------------------------------------------------
@@ -46,7 +50,7 @@ static int dp_ltc_solar_voltage_get (devp_t * param, void * value);
 
 static devp_t m_dp_ltc_solar_voltage =
 {
-    .name = "solar_voltage(mV)",
+    .name = "solar_voltage_(mV)",
     .type = DP_TYPE_UINT16,
     .size = sizeof(uint16_t),
     .persist = false,
@@ -56,17 +60,18 @@ static devp_t m_dp_ltc_solar_voltage =
 
 int dp_ltc_solar_voltage_get (devp_t * param, void * value)
 {
-    const bool is_ltc_initialized = ltc4015_init();
+    *((uint16_t*)value) = 0U;
 
-    if (true == is_ltc_initialized)
+    if(true == platform_i2c_request(RETARGET_I2C_DEV, 1000U))
     {
-        *((uint16_t*)value) = ltc4015_panelV_read();
-    }
-    else
-    {
-        *((uint16_t*)value) = 0U;
-    }
+        const bool is_ltc_initialized = ltc4015_init();
 
+        if (true == is_ltc_initialized)
+        {
+            *((uint16_t*)value) = ltc4015_panelV_read();
+        }
+        platform_i2c_release(RETARGET_I2C_DEV);
+    }
     return sizeof(uint16_t);
 }
 // -----------------------------------------------------------------------------
@@ -76,7 +81,7 @@ static int dp_ltc_solar_current_get (devp_t * param, void * value);
 
 static devp_t m_dp_ltc_solar_current =
 {
-    .name = "solar_current(mA)",
+    .name = "solar_current_(mA)",
     .type = DP_TYPE_INT16,
     .size = sizeof(int16_t),
     .persist = false,
@@ -86,17 +91,18 @@ static devp_t m_dp_ltc_solar_current =
 
 int dp_ltc_solar_current_get (devp_t * param, void * value)
 {
-    const bool is_ltc_initialized = ltc4015_init();
+    *((int16_t*)value) = 0;
 
-    if (true == is_ltc_initialized)
+    if(true == platform_i2c_request(RETARGET_I2C_DEV, 1000U))
     {
-        *((int16_t*)value) = ltc4015_charge_current_read();
-    }
-    else
-    {
-        *((int16_t*)value) = 0;
-    }
+        const bool is_ltc_initialized = ltc4015_init();
 
+        if (true == is_ltc_initialized)
+        {
+            *((int16_t*)value) = ltc4015_charge_current_read();
+        }
+        platform_i2c_release(RETARGET_I2C_DEV);
+    }
     return sizeof(int16_t);
 }
 // -----------------------------------------------------------------------------
