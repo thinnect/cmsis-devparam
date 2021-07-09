5ebce1be6533fc2a318240ea8cb4a3440526d349
Raido <raidopahtma@gmail.com>
2021-07-09 16:58:09 +0300

    Add parameter to show bootloader info.

---
+++ include/params/devp_bootloader.h
@@ -0,0 +1,14 @@
+/**
+ * Bootloader info through deviceparameters.
+ *
+ * Copyright Thinnect Inc. 2021
+ * @license MIT
+ */
+#ifndef DEVP_BOOTLOADER_H_
+#define DEVP_BOOTLOADER_H_
+
+#include <stdint.h>
+
+void devp_bootloader_init ();
+
+#endif//DEVP_BOOTLOADER_H_
