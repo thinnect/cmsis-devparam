86b3450e86221488d2dbc30fb269700ffc7e54ed
Raido <raidopahtma@gmail.com>
2021-11-26 17:28:44 +0200

    Refactor locking and use pool for outgoing.

--- src/lib/devp_comms.c
+++ src/lib/devp_comms.c
@@ -8,8 +8,7 @@
 #include <string.h>
 
 #include "devp.h"
-
-#include "mist_comm.h"
+#include "devp_comms.h"
 
 #include "endianness.h"
 
@@ -19,8 +18,9 @@
 #include "log.h"
 
 #define DEVP_FLAGS_RX    (1 << 0)
-#define DEVP_FLAGS_SLEEP (1 << 1)
-#define DEVP_FLAGS_ALL 0x7FFFFFFF
+#define DEVP_FLAGS_SENT  (1 << 1)
+#define DEVP_FLAGS_SLEEP (1 << 2)
+#define DEVP_FLAGS_ALL   (   0x7)
 
 #ifndef DEVP_MAX_IFACES
 #define DEVP_MAX_IFACES 3
@@ -31,16 +31,19 @@
 #endif//DEVP_MAX_VALUE_SIZE
 
 #ifndef DEVP_SLEEP_TIMEOUT_MS
-#define DEVP_SLEEP_TIMEOUT_MS 5000
+#define DEVP_SLEEP_TIMEOUT_MS 30000
 #endif//DEVP_SLEEP_TIMEOUT_MS
 
-static osMutexId_t m_mutex;
 static osThreadId_t m_devp_thread_id;
 static osTimerId_t m_sleep_timer;
-static comms_msg_t m_msg;
-static bool m_busy;
-static comms_layer_t * m_rx_iface;
 
+static osMutexId_t m_rx_mutex;
+static comms_msg_t m_rx_msg; // We keep a dedicated message for RX purpose and do not use the pool
+static comms_layer_t * mp_rx_iface;
+
+static comms_pool_t * mp_pool;
+
+static osMutexId_t m_rcvr_mutex;
 static comms_layer_t * mp_ifaces[DEVP_MAX_IFACES];
 static comms_sleep_controller_t * mp_sleep_ctrl[DEVP_MAX_IFACES];
 static bool m_heartbeat[DEVP_MAX_IFACES];
@@ -59,158 +62,180 @@ static void memcpy_swap (void * dst, void * src, size_t length)
 	#endif//__LITTLE_ENDIAN__
 }
 
-static void radio_status_changed(comms_layer_t* comms, comms_status_t status, void* user)
+
+static void radio_status_changed(comms_layer_t * comms, comms_status_t status, void * user)
 {
     // Actual status change is checked by polling during startup, but the callback is mandatory
 }
 
-static void receive_message (comms_layer_t * comms, const comms_msg_t* msg, void * user)
+
+static void receive_message (comms_layer_t * comms, const comms_msg_t * msg, void * user)
 {
-	while(osOK != osMutexAcquire(m_mutex, osWaitForever));
-	if (false == m_busy)
+	while (osOK != osMutexAcquire(m_rx_mutex, osWaitForever));
+	if (NULL == mp_rx_iface)
 	{
-		memcpy(&m_msg, msg, sizeof(comms_msg_t));
-		m_rx_iface = comms;
-		m_busy = true;
-		osThreadFlagsSet(m_devp_thread_id, DEVP_FLAGS_RX);
+		memcpy(&m_rx_msg, msg, sizeof(comms_msg_t));
+		mp_rx_iface = comms;
 	}
-	osMutexRelease(m_mutex);
+	osMutexRelease(m_rx_mutex);
+
+	osThreadFlagsSet(m_devp_thread_id, DEVP_FLAGS_RX);
 }
+
 
 static void msg_send_done (comms_layer_t * comms, comms_msg_t * msg, comms_error_t result, void * user)
 {
     logger(result == COMMS_SUCCESS ? LOG_DEBUG1: LOG_WARN1, "snt %u", result);
-    while(osOK != osMutexAcquire(m_mutex, osWaitForever));
-    m_busy = false;
-    osMutexRelease(m_mutex);
+    osThreadFlagsSet(m_devp_thread_id, DEVP_FLAGS_SENT);
 }
 
-static comms_error_t errorSeqnum(comms_layer_t * comms, const comms_address_t * destination,
+
+static comms_msg_t * errorSeqnum(comms_layer_t * comms, const comms_address_t * destination,
                                  bool exists, int error, uint8_t seqnum)
 {
-	comms_init_message(comms, &m_msg);
-	comms_set_destination(comms, &m_msg, destination);
-
-	dp_error_parameter_seqnum_t* ep =
-	    (dp_error_parameter_seqnum_t*)comms_get_payload(comms, &m_msg, sizeof(dp_error_parameter_seqnum_t));
-	if (NULL != ep)
+	comms_msg_t * p_msg = comms_pool_get(mp_pool, 0);
+	if (NULL != p_msg)
 	{
-		ep->header = DP_ERROR_PARAMETER_SEQNUM;
-		ep->exists = exists;
-		ep->error = error;
-		ep->seqnum = seqnum;
+		comms_init_message(comms, p_msg);
+		comms_set_destination(comms, p_msg, destination);
 
-		comms_set_payload_length(comms, &m_msg, sizeof(dp_error_parameter_seqnum_t));
+		dp_error_parameter_seqnum_t* ep =
+		    (dp_error_parameter_seqnum_t*)comms_get_payload(comms, p_msg, sizeof(dp_error_parameter_seqnum_t));
+		if (NULL != ep)
+		{
+			ep->header = DP_ERROR_PARAMETER_SEQNUM;
+			ep->exists = exists;
+			ep->error = error;
+			ep->seqnum = seqnum;
 
-		comms_error_t err = comms_send(comms, &m_msg, msg_send_done, NULL);
-		logger(COMMS_SUCCESS == err ? LOG_DEBUG1: LOG_WARN1, "esnd %u", err);
-		return err;
+			comms_set_payload_length(comms, p_msg, sizeof(dp_error_parameter_seqnum_t));
+
+			return p_msg;
+		}
+
+		comms_pool_put(mp_pool, p_msg);
 	}
-	return COMMS_FAIL;
+	else warn1("pool");
+
+	return NULL;
 }
 
-static comms_error_t errorId(comms_layer_t * comms, const comms_address_t * destination,
-                             bool exists, int error, const char * idstr, uint8_t idlen)
+
+static comms_msg_t * errorId (comms_layer_t * comms, const comms_address_t * destination,
+                              bool exists, int error, const char * idstr, uint8_t idlen)
 {
-	comms_init_message(comms, &m_msg);
-	comms_set_destination(comms, &m_msg, destination);
-
-	dp_error_parameter_id_t* ep =
-	    (dp_error_parameter_id_t*)comms_get_payload(comms, &m_msg, sizeof(dp_error_parameter_id_t) + idlen);
-	if (NULL != ep)
+	comms_msg_t * p_msg = comms_pool_get(mp_pool, 0);
+	if (NULL != p_msg)
 	{
-		ep->header = DP_ERROR_PARAMETER_ID;
-		ep->exists = exists;
-		ep->error = error;
-		ep->idlength = idlen;
-		memcpy(ep->id, idstr, idlen);
+		comms_init_message(comms, p_msg);
+		comms_set_destination(comms, p_msg, destination);
 
-		comms_set_payload_length(comms, &m_msg, sizeof(dp_error_parameter_id_t) + idlen);
+		dp_error_parameter_id_t* ep =
+		    (dp_error_parameter_id_t*)comms_get_payload(comms, p_msg, sizeof(dp_error_parameter_id_t) + idlen);
+		if (NULL != ep)
+		{
+			ep->header = DP_ERROR_PARAMETER_ID;
+			ep->exists = exists;
+			ep->error = error;
+			ep->idlength = idlen;
+			memcpy(ep->id, idstr, idlen);
 
-		comms_error_t err = comms_send(comms, &m_msg, msg_send_done, NULL);
-		logger(COMMS_SUCCESS == err ? LOG_DEBUG1: LOG_WARN1, "esnd %u", err);
-		return err;
+			comms_set_payload_length(comms, p_msg, sizeof(dp_error_parameter_id_t) + idlen);
+
+			return p_msg;
+		}
+
+		comms_pool_put(mp_pool, p_msg);
 	}
-	return COMMS_FAIL;
+	else warn1("pool");
+
+	return NULL;
 }
 
-static comms_error_t sendValue(comms_layer_t * comms, const comms_address_t * destination,
+
+static comms_msg_t * sendValue(comms_layer_t * comms, const comms_address_t * destination,
                                const char* fid, uint8_t idx, uint8_t tp, void* value, uint8_t length)
 {
 	debugb3("dp.v[%u] %s", value, length, idx, fid);
-
-	comms_init_message(comms, &m_msg);
-	comms_set_destination(comms, &m_msg, destination);
-
-	uint8_t idlen = strlen(fid);
-	dp_parameter_t* df =
-	    (dp_parameter_t*) comms_get_payload(comms, &m_msg, sizeof(dp_parameter_t) + idlen + length);
-	if (NULL != df)
+	comms_msg_t * p_msg = comms_pool_get(mp_pool, 0);
+	if (NULL != p_msg)
 	{
-		df->header = DP_PARAMETER;
-		df->type = tp;
-		df->seqnum = idx;
-		df->idlength = idlen;
-		df->valuelength = length;
-		memcpy((uint8_t*)df->id, fid, idlen);
-		if(length > 0)
+		comms_init_message(comms, p_msg);
+		comms_set_destination(comms, p_msg, destination);
+
+		uint8_t idlen = strlen(fid);
+		dp_parameter_t* df =
+		    (dp_parameter_t*) comms_get_payload(comms, p_msg, sizeof(dp_parameter_t) + idlen + length);
+		if (NULL != df)
 		{
-			void * pv = (void*)(((uint8_t*)(df->id)) + idlen); // Pointer to value location
-			switch(tp)
+			df->header = DP_PARAMETER;
+			df->type = tp;
+			df->seqnum = idx;
+			df->idlength = idlen;
+			df->valuelength = length;
+			memcpy((uint8_t*)df->id, fid, idlen);
+			if(length > 0)
 			{
-				case DP_TYPE_UINT16:
-				case DP_TYPE_INT16:
-					memcpy_swap(pv, value, sizeof(uint16_t));
-				break;
+				void * pv = (void*)(((uint8_t*)(df->id)) + idlen); // Pointer to value location
+				switch(tp)
+				{
+					case DP_TYPE_UINT16:
+					case DP_TYPE_INT16:
+						memcpy_swap(pv, value, sizeof(uint16_t));
+					break;
 
-				case DP_TYPE_UINT32:
-				case DP_TYPE_INT32:
-					memcpy_swap(pv, value, sizeof(uint32_t));
-				break;
+					case DP_TYPE_UINT32:
+					case DP_TYPE_INT32:
+						memcpy_swap(pv, value, sizeof(uint32_t));
+					break;
 
-				case DP_TYPE_UINT64:
-				case DP_TYPE_INT64:
-					memcpy_swap(pv, value, sizeof(uint64_t));
-				break;
+					case DP_TYPE_UINT64:
+					case DP_TYPE_INT64:
+						memcpy_swap(pv, value, sizeof(uint64_t));
+					break;
 
-				case DP_ARRAY_UINT16:
-				case DP_ARRAY_INT16:
-					for(int i=0;i<length/sizeof(uint16_t);i++)
-					{
-						memcpy_swap(((uint16_t*)pv)+i, ((uint16_t*)value)+i, sizeof(uint16_t));
-					}
-				break;
+					case DP_ARRAY_UINT16:
+					case DP_ARRAY_INT16:
+						for(int i=0;i<length/sizeof(uint16_t);i++)
+						{
+							memcpy_swap(((uint16_t*)pv)+i, ((uint16_t*)value)+i, sizeof(uint16_t));
+						}
+					break;
 
-				case DP_ARRAY_UINT32:
-				case DP_ARRAY_INT32:
-					for(int i=0;i<length/sizeof(uint32_t);i++)
-					{
-						memcpy_swap(((uint32_t*)pv)+i, ((uint32_t*)value)+i, sizeof(uint32_t));
-					}
-				break;
+					case DP_ARRAY_UINT32:
+					case DP_ARRAY_INT32:
+						for(int i=0;i<length/sizeof(uint32_t);i++)
+						{
+							memcpy_swap(((uint32_t*)pv)+i, ((uint32_t*)value)+i, sizeof(uint32_t));
+						}
+					break;
 
-				case DP_ARRAY_UINT64:
-				case DP_ARRAY_INT64:
-					for(int i=0;i<length/sizeof(uint64_t);i++)
-					{
-						memcpy_swap(((uint64_t*)pv)+i, ((uint64_t*)value)+i, sizeof(uint64_t));
-					}
-				break;
+					case DP_ARRAY_UINT64:
+					case DP_ARRAY_INT64:
+						for(int i=0;i<length/sizeof(uint64_t);i++)
+						{
+							memcpy_swap(((uint64_t*)pv)+i, ((uint64_t*)value)+i, sizeof(uint64_t));
+						}
+					break;
 
-				default:
-					memcpy(pv, value, length);
-				break;
+					default:
+						memcpy(pv, value, length);
+					break;
+				}
 			}
+
+			comms_set_payload_length(comms, p_msg, sizeof(dp_parameter_t) + idlen + length);
+
+			return p_msg;
 		}
 
-		comms_set_payload_length(comms, &m_msg, sizeof(dp_parameter_t) + idlen + length);
-
-		comms_error_t err = comms_send(comms, &m_msg, msg_send_done, NULL);
-		logger(err == COMMS_SUCCESS ? LOG_DEBUG1: LOG_WARN1, "vsnd [%02X]%s (%u)", idx, fid, err);
-		return err;
+		comms_pool_put(mp_pool, p_msg);
 	}
-	return COMMS_FAIL;
+	else warn1("pool");
+
+	return NULL;
 }
+
 
 // Perform network-to-host transformation for select numeric types
 static int setValue(const char * name, DeviceParameterTypes_t tp, void * value, uint8_t size)
@@ -317,15 +342,15 @@ static int setValue(const char * name, DeviceParameterTypes_t tp, void * value, 
 }
 
 
-static comms_error_t handle_rx()
+static comms_msg_t * handle_rx (comms_layer_t * p_comms, comms_msg_t * p_rx_msg)
 {
 	comms_address_t client;
-	uint8_t len = comms_get_payload_length(m_rx_iface, &m_msg);
-	uint8_t * payload = (uint8_t*)comms_get_payload(m_rx_iface, &m_msg, len);
+	uint8_t len = comms_get_payload_length(p_comms, p_rx_msg);
+	uint8_t * payload = (uint8_t*)comms_get_payload(p_comms, p_rx_msg, len);
 
-	comms_get_source(m_rx_iface, &m_msg, &client);
+	comms_get_source(p_comms, p_rx_msg, &client);
 
-	debugb1("rcv[%p]", payload, len, m_rx_iface);
+	debugb1("rcv[%p]", payload, len, p_comms);
 	if ((len > 0)&&(NULL != payload))
 	{
 		switch(((uint8_t*)payload)[0])
@@ -347,17 +372,17 @@ static comms_error_t handle_rx()
 					int size = devp_discover_idx(gf->seqnum, &name, &tp, value, sizeof(value));
 					if(0 <= size)
 					{
-						return sendValue(m_rx_iface, &client, name, gf->seqnum, tp, value, size);
+						return sendValue(p_comms, &client, name, gf->seqnum, tp, value, size);
 					}
 					else
 					{
-						return errorSeqnum(m_rx_iface, &client, DEVP_UNKNOWN != size, -DEVP_UNKNOWN, gf->seqnum);
+						return errorSeqnum(p_comms, &client, DEVP_UNKNOWN != size, -DEVP_UNKNOWN, gf->seqnum);
 					}
 				}
 				else // Bad packet size
 				{
 					errb1("len", payload, len);
-					return errorId(m_rx_iface, &client, false, -DEVP_UNKNOWN, "BAD REQ LEN", 11);
+					return errorId(p_comms, &client, false, -DEVP_UNKNOWN, "BAD REQ LEN", 11);
 				}
 			break;
 
@@ -379,23 +404,23 @@ static comms_error_t handle_rx()
 						int size = devp_discover_name(name, &idx, &tp, (void*)value, sizeof(value));
 						if(0 <= size)
 						{
-							return sendValue(m_rx_iface, &client, name, idx, tp, value, size);
+							return sendValue(p_comms, &client, name, idx, tp, value, size);
 						}
 						else // No such parameter found
 						{
-							return errorId(m_rx_iface, &client, DEVP_UNKNOWN != size, -size, name, gf->idlength);
+							return errorId(p_comms, &client, DEVP_UNKNOWN != size, -size, name, gf->idlength);
 						}
 					}
 					else // No such parameter found
 					{
 						err1("id len %d", (int)gf->idlength);
-						return errorId(m_rx_iface, &client, false, -DEVP_UNKNOWN, "BAD ID LEN", 10);
+						return errorId(p_comms, &client, false, -DEVP_UNKNOWN, "BAD ID LEN", 10);
 					}
 				}
 				else // Bad packet size
 				{
 					errb1("len", payload, len);
-					return errorId(m_rx_iface, &client, false, -DEVP_UNKNOWN, "BAD REQ LEN", 11);
+					return errorId(p_comms, &client, false, -DEVP_UNKNOWN, "BAD REQ LEN", 11);
 				}
 			break;
 
@@ -426,36 +451,36 @@ static comms_error_t handle_rx()
 									size = devp_discover_name(name, &idx, &tp, (void*)value, sizeof(value));
 									if(0 <= size)
 									{
-										return sendValue(m_rx_iface, &client, name, idx, tp, value, size);
+										return sendValue(p_comms, &client, name, idx, tp, value, size);
 									}
 									else // Rather unexpected to fail here, but possible
 									{
 										warnb1("s %u", value, sf->valuelength, idx);
-										return errorId(m_rx_iface, &client, true, -size, name, sf->idlength);
+										return errorId(p_comms, &client, true, -size, name, sf->idlength);
 									}
 								}
 								else
 								{
 									warnb1("s %u", value, sf->valuelength, idx);
-									return errorId(m_rx_iface, &client, true, -size, name, sf->idlength);
+									return errorId(p_comms, &client, true, -size, name, sf->idlength);
 								}
 							}
 							else
 							{
 								warnb1("s %u", value, sf->valuelength, idx);
-								return errorId(m_rx_iface, &client, DEVP_UNKNOWN != size, -size, name, sf->idlength);
+								return errorId(p_comms, &client, DEVP_UNKNOWN != size, -size, name, sf->idlength);
 							}
 						}
 						else // Bad id len
 						{
 							err1("id len %d", (int)sf->idlength);
-							return errorId(m_rx_iface, &client, false, -DEVP_UNKNOWN, "BAD ID LEN", 10);
+							return errorId(p_comms, &client, false, -DEVP_UNKNOWN, "BAD ID LEN", 10);
 						}
 					}
 					else // Bad packet size
 					{
 						errb1("len", payload, len);
-						return errorId(m_rx_iface, &client, false, -DEVP_UNKNOWN, "BAD REQ LEN", 11);
+						return errorId(p_comms, &client, false, -DEVP_UNKNOWN, "BAD REQ LEN", 11);
 					}
 				}
 			break;
@@ -464,65 +489,124 @@ static comms_error_t handle_rx()
 			break;
 		}
 	}
-	return COMMS_FAIL;
+	return NULL;
 }
 
-static void devp_comms_loop()
+
+static void sleep_block_comms (comms_layer_t * p_comms)
 {
+	while(osOK != osMutexAcquire(m_rcvr_mutex, osWaitForever));
+	for (int i=0;i<DEVP_MAX_IFACES;i++)
+	{
+		if(p_comms == mp_ifaces[i])
+		{
+			if(NULL != mp_sleep_ctrl[i]) // Sleep control is optional
+			{
+				debug1("block %d", i);
+				comms_sleep_block(mp_sleep_ctrl[i]);
+				osTimerStart(m_sleep_timer, DEVP_SLEEP_TIMEOUT_MS);
+			}
+			break;
+		}
+	}
+	osMutexRelease(m_rcvr_mutex);
+}
+
+
+static void sleep_allow_comms (void)
+{
+	while(osOK != osMutexAcquire(m_rcvr_mutex, osWaitForever));
+	for(int i=0;i<DEVP_MAX_IFACES;i++)
+	{
+		if (NULL != mp_sleep_ctrl[i])
+		{
+			debug1("sleep %d", i);
+			comms_sleep_allow(mp_sleep_ctrl[i]);
+		}
+	}
+	osMutexRelease(m_rcvr_mutex);
+}
+
+
+static void devp_comms_loop (void * arg)
+{
+	comms_msg_t * p_msg = NULL;
 	for(;;)
 	{
 		uint32_t flags = osThreadFlagsWait(DEVP_FLAGS_ALL, osFlagsWaitAny, osWaitForever);
-		if (flags & DEVP_FLAGS_RX)
-		{
-			while(osOK != osMutexAcquire(m_mutex, osWaitForever));
-			if (COMMS_SUCCESS != handle_rx())
-			{
-				m_busy = false;
-			}
-			else // Something is happening, keep the interface awake for a bit
-			{
-				for (int i=0;i<DEVP_MAX_IFACES;i++)
-				{
-					if(m_rx_iface == mp_ifaces[i])
-					{
-						if(NULL != mp_sleep_ctrl[i]) // Sleep control is optional
-						{
-							debug1("block %d", i);
-							comms_sleep_block(mp_sleep_ctrl[i]);
-							osTimerStart(m_sleep_timer, DEVP_SLEEP_TIMEOUT_MS);
-						}
-						break;
-					}
-				}
 
-			}
-			osMutexRelease(m_mutex);
+		if (flags & DEVP_FLAGS_SENT)
+		{
+			comms_pool_put(mp_pool, p_msg);
+			p_msg = NULL;
 		}
+
+		if (NULL == p_msg)
+		{
+			comms_layer_t * p_comms = NULL;
+			while(osOK != osMutexAcquire(m_rx_mutex, osWaitForever));
+			if (NULL != mp_rx_iface)
+			{
+				p_comms = mp_rx_iface;
+				p_msg = handle_rx(mp_rx_iface, &m_rx_msg);
+				mp_rx_iface = NULL;
+			}
+			osMutexRelease(m_rx_mutex);
+
+			if (NULL != p_msg) // Something is happening, keep the interface awake for a bit
+			{
+				sleep_block_comms(p_comms);
+
+				comms_error_t err = comms_send(p_comms, p_msg, msg_send_done, NULL);
+				logger(COMMS_SUCCESS == err ? LOG_DEBUG1: LOG_WARN1, "snd %u", err);
+				if (err != COMMS_SUCCESS)
+				{
+					comms_pool_put(mp_pool, p_msg);
+					p_msg = NULL;
+				}
+			}
+		}
+
 		if (flags & DEVP_FLAGS_SLEEP) // All interfaces put to sleep together
 		{
-			for(int i=0;i<DEVP_MAX_IFACES;i++)
-			{
-				if (NULL != mp_sleep_ctrl[i])
-				{
-					debug1("sleep %d", i);
-					comms_sleep_allow(mp_sleep_ctrl[i]);
-				}
-			}
+			sleep_allow_comms();
 		}
 	}
 }
+
 
 static void sleep_timer_fired_cb()
 {
 	osThreadFlagsSet(m_devp_thread_id, DEVP_FLAGS_SLEEP);
 }
 
-bool devp_comms_init()
-{
-	m_mutex = osMutexNew(NULL);
-	m_sleep_timer = osTimerNew(sleep_timer_fired_cb, osTimerOnce, NULL, NULL);
 
-	m_busy = false;
+bool devp_comms_init (comms_pool_t * p_pool)
+{
+	mp_pool = p_pool;
+
+	const osMutexAttr_t mutex_param = { .attr_bits = osMutexPrioInherit };
+
+	m_rx_mutex = osMutexNew(&mutex_param);
+	if (NULL == m_rx_mutex)
+	{
+		return false;
+	}
+
+	m_rcvr_mutex = osMutexNew(&mutex_param);
+	if (NULL == m_rcvr_mutex)
+	{
+		osMutexDelete(m_rx_mutex);
+		return false;
+	}
+
+	m_sleep_timer = osTimerNew(sleep_timer_fired_cb, osTimerOnce, NULL, NULL);
+	if (NULL == m_sleep_timer)
+	{
+		osMutexDelete(m_rcvr_mutex);
+		osMutexDelete(m_rx_mutex);
+		return false;
+	}
 
 	for(int i=0;i<DEVP_MAX_IFACES;i++)
 	{
@@ -535,15 +619,19 @@ bool devp_comms_init()
 
     if (NULL == m_devp_thread_id)
     {
+    	osTimerDelete(m_sleep_timer);
+		osMutexDelete(m_rcvr_mutex);
+		osMutexDelete(m_rx_mutex);
     	return false;
     }
     return true;
 }
 
+
 int devp_add_iface(comms_layer_t * comms, comms_sleep_controller_t * ctrl, bool heartbeat)
 {
 	int ret = -1;
-	while(osOK != osMutexAcquire(m_mutex, osWaitForever));
+	while(osOK != osMutexAcquire(m_rcvr_mutex, osWaitForever));
 	for(int i=0;i<DEVP_MAX_IFACES;i++)
 	{
 		if(mp_ifaces[i] == comms)
@@ -567,14 +655,15 @@ int devp_add_iface(comms_layer_t * comms, comms_sleep_controller_t * ctrl, bool 
 			break;
 		}
 	}
-	osMutexRelease(m_mutex);
+	osMutexRelease(m_rcvr_mutex);
 	return ret;
 }
+
 
 int devp_remove_iface(comms_layer_t * comms)
 {
 	int ret = -1;
-	while(osOK != osMutexAcquire(m_mutex, osWaitForever));
+	while(osOK != osMutexAcquire(m_rcvr_mutex, osWaitForever));
 	for(int i=0;i<DEVP_MAX_IFACES;i++)
 	{
 		if(mp_ifaces[i] == comms)
@@ -592,6 +681,6 @@ int devp_remove_iface(comms_layer_t * comms)
 			break;
 		}
 	}
-	osMutexRelease(m_mutex);
+	osMutexRelease(m_rcvr_mutex);
 	return ret;
 }
