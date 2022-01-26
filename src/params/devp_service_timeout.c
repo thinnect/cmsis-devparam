aa9b6544b26f81603354f48ece85625cbe35f746
Lembitu Valdmets <lembitu.valdmets@thinnect.com>
2022-01-26 17:30:40 +0200

    [TS-258] gave deviceparameter struct its rightful name

--- src/params/devp_service_timeout.c
+++ src/params/devp_service_timeout.c
@@ -12,12 +12,12 @@
 #define DEVP_SERVICE_TIMEOUT_MS 5000
 #endif//DEVP_SERVICE_TIMEOUT_MS
 
-static uint32_t m_scd_default_asc;
+static uint32_t m_service_mode_timeout;
 
 static int dp_service_mode_timeout_get (devp_t * param, void * value);
 static int dp_service_mode_timeout_set (devp_t * param, bool init, const void * value, uint8_t size);
 
-static devp_t m_dp_scd_default_asc = {
+static devp_t m_dp_service_mode_timeout = {
 	.name = "service_mode_timeout",
 	.type = DP_TYPE_UINT32,
 	.size = sizeof(uint32_t),
@@ -28,23 +28,23 @@ static devp_t m_dp_scd_default_asc = {
 
 static int dp_service_mode_timeout_get (devp_t * param, void * value)
 {
-	*((uint32_t*)value) = m_scd_default_asc;
+	*((uint32_t*)value) = m_service_mode_timeout;
 	return sizeof(uint32_t);
 }
 
 static int dp_service_mode_timeout_set (devp_t * param, bool init, const void * value, uint8_t size)
 {
-	m_scd_default_asc = *((uint32_t*)value);
+	m_service_mode_timeout = *((uint32_t*)value);
 	return sizeof(uint32_t);
 }
 
 void devp_service_mode_timeout_init ()
 {
-    m_scd_default_asc = DEVP_SERVICE_TIMEOUT_MS;
-    devp_register(&m_dp_scd_default_asc);
+    m_service_mode_timeout = DEVP_SERVICE_TIMEOUT_MS;
+    devp_register(&m_dp_service_mode_timeout);
 }
 
 uint32_t devp_service_mode_timeout_get ()
 {
-    return m_scd_default_asc;
+    return m_service_mode_timeout;
 }
