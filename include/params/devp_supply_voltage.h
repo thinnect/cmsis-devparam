1099ae846de0e720cf6b2502cc7855278abd258c
Lembitu Valdmets <lembitu.valdmets@thinnect.com>
2022-10-13 15:51:24 +0300

    [TS-1055] Added initial functionality to read batery voltage through deviceparameter. tested successfully on ThinboxCO2

---
+++ include/params/devp_supply_voltage.h
@@ -0,0 +1,8 @@
+#ifndef DEVP_SUPPLY_VOLTAGE_H_
+#define DEVP_SUPPLY_VOLTAGE_H_
+
+#include "devp.h"
+
+void devp_supply_voltage_init ();
+
+#endif
---
+++ src/params/devp_supply_voltage.c
@@ -0,0 +1,40 @@
+#include "devp_supply_voltage.h"
+
+#include "sensors.h"
+#include "stdint.h"
+
+static int dp_battery_voltage_get (devp_t * param, void * value);
+
+static devp_t m_dp_battery_voltage =
+{
+    .name = "battery_vol_mW",
+    .type = DP_TYPE_UINT16,
+    .size = sizeof(uint16_t),
+    .persist = false,
+    .getf = dp_battery_voltage_get,
+    .setf = 0
+};
+
+int dp_battery_voltage_get (devp_t * param, void * value)
+{
+    *((uint16_t*)value) = 0U;
+    uint16_t milliVolts;
+	#ifdef DIRECT_BATTERY_SUPPLY
+	{
+		SupplyVoltageReader_init();
+		milliVolts = SupplyVoltageReader_read();
+		SupplyVoltageReader_deinit();
+	}
+	#else
+	{
+		read_battery_voltage(&milliVolts);
+	}
+	#endif
+    *((uint16_t*)value) = milliVolts;
+    return sizeof(uint16_t);
+}
+
+void devp_supply_voltage_init ()
+{
+    devp_register(&m_dp_battery_voltage);
+}
