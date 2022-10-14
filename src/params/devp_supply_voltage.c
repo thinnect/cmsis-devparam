147404410c50951abcfbe068be46fe687a4fa5cc
Lembitu Valdmets <lembitu.valdmets@thinnect.com>
2022-10-14 11:26:55 +0300

    [TS-1055] Fixed typo

--- src/params/devp_supply_voltage.c
+++ src/params/devp_supply_voltage.c
@@ -17,7 +17,7 @@ static int dp_battery_voltage_get (devp_t * param, void * value);
 
 static devp_t m_dp_battery_voltage =
 {
-    .name = "battery_vol_mW",
+    .name = "battery_vol_mV",
     .type = DP_TYPE_UINT16,
     .size = sizeof(uint16_t),
     .persist = false,
